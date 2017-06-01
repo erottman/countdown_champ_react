18e1819439f715ec70b0d3c37caea36ab86c2304
Erica Rottman <erica7650@gmail.com>
2017-06-01 16:29:24 -0700

    set initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 import './App.css';
 
 class App extends Component {
@@ -20,17 +21,14 @@ class App extends Component {
       <div className="App">
       <div className="App-title">
         Countdown to {this.state.deadline}
-      </div>
-      <div>
-        <div className="Clock-days">14 days</div>
-        <div className="Clock-hours">30 hours</div>
-        <div className="Clock-minutes">15 minutes</div>
-        <div className="Clock-seconds">20 seconds</div>
-      </div>
+        </div>
+        <Clock
+          deadline={this.state.deadline}
+        />
       <div>
         <input placeholder='new date'
         onChange={event => this.setState({newDeadline: event.target.value})}
-      />
+        />
         <button onClick={() => this.changeDeadline()}>
           Submit
         </button>
