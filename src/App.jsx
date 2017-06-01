b3b5c71e2d82321b7c02f98196d58b427c033bc3
Erica Rottman <erica7650@gmail.com>
2017-06-01 12:57:49 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <div>Countdown to December 25, 2017</div>
+      <div>
+        <div>14 Days</div>
+        <div>30 hours</div>
+        <div>15 minutes</div>
+        <div>20 seconds</div>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default App;
